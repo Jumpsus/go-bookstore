0852e5c43a6fcb97286b63fc22d77f77b65d628d
Jumpsus <peerapat.rs@gmail.com>
2022-05-11 19:56:28 +0700

    save

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -2,7 +2,8 @@ package config
 
 import(
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	//_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var (
@@ -10,7 +11,8 @@ var (
 )
 
 func Connect(){
-	d, err := gorm.Open("mysql", "root:0000/simplerest?charset=utf8&parseTime=True&loc=Local")
+	//d, err := gorm.Open("mysql", "root:0000/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("sqlite3", "bookstore.db")
 	if err != nil{
 		panic(err)
 	}
