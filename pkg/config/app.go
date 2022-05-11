149131ab65d7a1f82b1da0ca61746d668ae3716b
Jumpsus <peerapat.rs@gmail.com>
2022-05-11 16:46:19 +0700

    save

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect(){
-	d, err := gorm.Open("mysql", "Jumpsus:Password/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "pratanac:JumpPassword0!/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil{
 		panic(err)
 	}
